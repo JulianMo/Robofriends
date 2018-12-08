45a7f0badbbad8ff414e459d46d2912f00978f32
julian <julian_moller2@hotmail.com>
2018-12-08 01:20:16 +0100

    Robofriends 1.0
    Build and files for RoboFriendsApp

---
+++ build/precache-manifest.e37efc3264e1a2985d289c2d3928e6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16d2c666bc4cc8a24e0f",
+    "url": "/static/js/main.16d2c666.chunk.js"
+  },
+  {
+    "revision": "49c7f43566752495a577",
+    "url": "/static/js/1.49c7f435.chunk.js"
+  },
+  {
+    "revision": "16d2c666bc4cc8a24e0f",
+    "url": "/static/css/main.6dac532d.chunk.css"
+  },
+  {
+    "revision": "49c7f43566752495a577",
+    "url": "/static/css/1.f97af4e7.chunk.css"
+  },
+  {
+    "revision": "cca081b8a5cf5cd4c22e82867139458a",
+    "url": "/index.html"
+  }
+];
